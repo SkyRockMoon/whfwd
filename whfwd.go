5b2bf10355df6846d9d0283354928b860d630805
Ryan Lowe <rdlowe@ara.com>
2017-08-25 01:39:45 -0600

    dirty, but working version

--- whfwd.go
+++ whfwd.go
@@ -1,3 +1,6 @@
+//Prior to runing start ngrok: ngrok http 8080
+// open ngrok viewer: http://127.0.0.1:4040/inspect/http
+
 package main
 
 import (
@@ -6,35 +9,56 @@ import (
 	"log"
 	"net/http"
 	//"os"
-	"encoding/json"
+	//"encoding/json"
 	"io/ioutil"
+	"github.com/buger/jsonparser"
+	"bytes"
 )
 
 func handleWebhook(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("=========================================================\n")
-	bodyBytes,_ := ioutil.ReadAll(r.Body)
-	//fmt.Println(bodyBytes)
-    	bodyString := string(bodyBytes)	
-	fmt.Println(bodyString)
+	data,_ := ioutil.ReadAll(r.Body)
+	
+	val,dataType,offset,err:=jsonparser.Get(data, "pullrequest", "author", "display_name")
+	display_name := string(val)
+	fmt.Println(display_name, dataType, offset, err)
+
+	val,dataType,offset,err=jsonparser.Get(data, "pullrequest", "title")
+	title := string(val)
+	fmt.Println(string(title), dataType, offset, err)
+
+	val,dataType,offset,err =jsonparser.Get(data, "pullrequest", "links", "self","href")
+	prUrl := string(val)
+	fmt.Println(prUrl)
+
 	fmt.Println("=========================================================\n")
 
-	webhookData := make(map[string]interface{})
-	json.NewDecoder(r.Body).Decode(&webhookData)
+	
+	//message := fmt.Sprintf("{\"activity\":\"New Pull Request\",\"body\":\"Name: %s\nTitle: %s\nLink: [Pull request](%s)\n\"}",display_name,title,link)
+	message := fmt.Sprintf(`{"activity":"New Pull Request","body":"Name: %s\nTitle: %s\nLink: [Pull request](%s)\n"}`,display_name,title,link)	
+	fmt.Println(message)
 
-	fmt.Println("got webhook payload: ")
-	for key, value := range webhookData {
-		fmt.Printf("%s : %v\n", key, value)
+	url := "https://hooks.glip.com/webhook/47c23015-f6af-452f-95bf-4052574a06da"
+	fmt.Println("URL:>", url)
+
+	var jsonStr = []byte(message)
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonStr))
+	req.Header.Set("X-Custom-Header", "myvalue")
+	req.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		panic(err)
 	}
+	defer resp.Body.Close()
+
+	fmt.Println("response Status:", resp.Status)
+	fmt.Println("response Headers:", resp.Header)
+	body, _ := ioutil.ReadAll(resp.Body)
+	fmt.Println("response Body:", string(body))
 	fmt.Println("=========================================================\n")
 
-
-	var f interface{}
-	json.Unmarshal(bodyBytes, &f)
-	m := f.(map[string]interface{})
-	for key, value := range m {
-		fmt.Printf("%s : %v\n",key, value)
-	}
-//	fmt.Println(pullRequestData)
 }
 
 func main() {
