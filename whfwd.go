47c9d7efc9e0efcb231d8952a73d50d092b5db2f
Ryan Lowe <rdlowe@ara.com>
2017-08-25 01:59:07 -0600

    cleaned and ready to transition to a real server

--- whfwd.go
+++ whfwd.go
@@ -1,51 +1,67 @@
-//Prior to runing start ngrok: ngrok http 8080
+// Web Socket Forwarding From Bitbucket to Glip
+//
+// Glip does not easily integrate with bitbucket. This is a hack to make new pull request notifications avalable to glip as a webhook.
+//
+// Ryan Lowe
+// rdlowe@ara.com
+//
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Prior to runing start ngrok: ngrok http 8080
+// This forwards bitbucket posts to localhost for testing. In general this is a security risk.
 // open ngrok viewer: http://127.0.0.1:4040/inspect/http
+//
+// Configure bitbucket to post to: http://localhost:8080/webhook (via ngrok), and enamble notification on the creation of a pull request
+//
+// Configure glip to listen for a post in the channel you want these messages shown in 
+//
+// In the comments below the "incoming" request is from bitbucket and the "outgoing" request goes to glip
 
 package main
 
 import (
+	"bytes"
 	"fmt"
-	//"io"
+	"github.com/buger/jsonparser"
+	"io/ioutil"
 	"log"
 	"net/http"
-	//"os"
-	//"encoding/json"
-	"io/ioutil"
-	"github.com/buger/jsonparser"
-	"bytes"
 )
 
 func handleWebhook(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("=========================================================\n")
-	data,_ := ioutil.ReadAll(r.Body)
-	
-	val,dataType,offset,err:=jsonparser.Get(data, "pullrequest", "author", "display_name")
+
+	// read body of incoming post request
+	data, _ := ioutil.ReadAll(r.Body)
+
+	// grab (pretty) display name of the user making the PR	
+	val,_,_,err:=jsonparser.Get(data, "pullrequest", "author", "display_name")
 	display_name := string(val)
-	fmt.Println(display_name, dataType, offset, err)
 
-	val,dataType,offset,err=jsonparser.Get(data, "pullrequest", "title")
+	// grab the title of the PR
+	val,_,_,err=jsonparser.Get(data, "pullrequest", "title")
 	title := string(val)
-	fmt.Println(string(title), dataType, offset, err)
 
-	val,dataType,offset,err =jsonparser.Get(data, "pullrequest", "links", "self","href")
+	// grab the link to the PR
+	val,_,_,err =jsonparser.Get(data, "pullrequest", "links", "self","href")
 	prUrl := string(val)
-	fmt.Println(prUrl)
-
-	fmt.Println("=========================================================\n")
-
 	
-	//message := fmt.Sprintf("{\"activity\":\"New Pull Request\",\"body\":\"Name: %s\nTitle: %s\nLink: [Pull request](%s)\n\"}",display_name,title,link)
-	message := fmt.Sprintf(`{"activity":"New Pull Request","body":"Name: %s\nTitle: %s\nLink: [Pull request](%s)\n"}`,display_name,title,link)	
-	fmt.Println(message)
+	// make JSON the hard way
+	//body is the only required field for a glip message
+	// [link](url) is a glip specific markup for an anchor
+	// other fields not used include: icon and title
+	message := fmt.Sprintf(`{"activity":"New Pull Request","body":"Name: %s\nTitle: %s\nLink: [Pull request](%s)\n"}`,display_name,title,prUrl)	
 
+	// url to send outgoing post request	
 	url := "https://hooks.glip.com/webhook/47c23015-f6af-452f-95bf-4052574a06da"
-	fmt.Println("URL:>", url)
 
+	// build outgoing request
 	var jsonStr = []byte(message)
 	req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonStr))
 	req.Header.Set("X-Custom-Header", "myvalue")
-	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Content-Type", "application/json") // this is a glip requirement
 
+	// make outgoing request
 	client := &http.Client{}
 	resp, err := client.Do(req)
 	if err != nil {
@@ -53,11 +69,13 @@ func handleWebhook(w http.ResponseWriter, r *http.Request) {
 	}
 	defer resp.Body.Close()
 
-	fmt.Println("response Status:", resp.Status)
-	fmt.Println("response Headers:", resp.Header)
-	body, _ := ioutil.ReadAll(resp.Body)
-	fmt.Println("response Body:", string(body))
-	fmt.Println("=========================================================\n")
+	// for debugging
+	//fmt.Println(message)	
+	//fmt.Println("response Status:", resp.Status)
+	//fmt.Println("response Headers:", resp.Header)
+	//body, _ := ioutil.ReadAll(resp.Body)
+	//fmt.Println("response Body:", string(body))
+	//fmt.Println("=========================================================\n")
 
 }
 
